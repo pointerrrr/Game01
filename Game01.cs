36aa1b51bef34e8a78cd1f21e4cf5cfd697c48d8
pointerrrr <gideonogilvie@gmail.com>
2016-11-10 13:48:26 +0100

    Initial commit

---
+++ Game01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game01
+{
+    public partial class Game01 : Form
+    {
+        public Game01()
+        {
+            InitializeComponent();
+            
+        }
+    }
+
+    public class CoolPanel : Panel
+    {
+        CoolPanel()
+        {
+            
+        }
+    }
+}
+
